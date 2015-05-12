117d396ef3a60c23481bec4991bb3e6b993cc164
Jeff Larson <thejefflarson@gmail.com>
2015-05-12 12:21:09 -0400

    only 255 keys, cuz type conversions

--- ht_test.c
+++ ht_test.c
@@ -27,7 +27,7 @@ main(){
   assert(NULL == ht_get(h, k3));
 
   clock_t t = clock();
-  for(int i = 0; i < 1000000; i++) {
+  for(int i = 1; i < 256; i++) {
     uint32_t *k = (uint32_t *)calloc(1, sizeof(uint32_t) + 1);
     *k = i;
     uint32_t *v = (uint32_t *)calloc(1, sizeof(uint32_t) + 1);
@@ -38,10 +38,9 @@ main(){
   printf("%f\n", (double) (clock() - t) / CLOCKS_PER_SEC);
 
   t = clock();
-  for(int i = 0; i < 1000000; i++) {
-    char k[16] = {0};
-    sprintf(k, "%d", i);
-    ht_get(h, k);
+  for(int i = 1; i < 256; i++) {
+    uint32_t *v = (uint32_t *)ht_get(h, (char *)&i);
+    assert(i == *v);
   }
   printf("%f\n", (double) (clock() - t) / CLOCKS_PER_SEC);
 
